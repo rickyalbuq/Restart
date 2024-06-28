d8918849c9c96fb6de211162bc6fdc96db87f18b
Henrique Albuquerque <ehf.albuquerque@gmail.com>
2024-06-28 10:16:15 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Henrique Albuquerque on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
